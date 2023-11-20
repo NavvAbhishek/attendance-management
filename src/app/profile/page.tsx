3bdb9a39a6fab323e90a6e210edb594f3a318e02
Navindu-Abhishek <abi2000navi@gmail.com>
2023-11-20 12:59:57 +0530

    add middleware

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,26 +1,43 @@
-"use client"
-import axios from "axios"
-import Link from "next/link"
-import { useRouter } from "next/navigation"
-import toast from "react-hot-toast"
-
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 const ProfilePage = () => {
-  const router = useRouter()
-  const logout = async ()=> {
+  const router = useRouter();
+  const [data, setData] = useState("nothing");
+  const logout = async () => {
     try {
-      await axios.get('/api/users/logout')
-      toast.success('Logout successful')
-      router.push('/login')
+      await axios.get("/api/users/logout");
+      toast.success("Logout successful");
+      router.push("/login");
     } catch (error: any) {
-      console.log(error.message)
-      toast.error(error.message)
+      console.log(error.message);
+      toast.error(error.message);
     }
-  }
+  };
+
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
 
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen'>
-      <h1 className='text-2xl'>Profile</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-2xl">Profile</h1>
+      <hr />
+      <h2 className="p-1 rounded bg-green-500 cursor-pointer">
+        {data === "nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>
+            {data}
+          </Link>
+        )}
+      </h2>
       <hr />
       <button
         type="button"
@@ -32,10 +49,17 @@ const ProfilePage = () => {
       >
         Logout
       </button>
+
+      <button
+        onClick={getUserDetails}
+        className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
+      >
+        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+          GetUser Details
+        </span>
+      </button>
     </div>
+  );
+};
 
-
-  )
-}
-
-export default ProfilePage
+export default ProfilePage;
