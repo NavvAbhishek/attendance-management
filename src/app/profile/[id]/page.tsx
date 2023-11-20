e6b352cbfc62f9587fdf2ec212615d0cd6f399be
Navindu-Abhishek <abi2000navi@gmail.com>
2023-11-20 12:59:57 +0530

    add middleware

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const UserProfile = ({params}: any) => {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen py-2'>
-        <h1>ProfilePage</h1>
+        <h1 className='pb-5 text-2xl'>ProfilePage</h1>
         <hr />
         <p className="text-4xl">
             Profile Page 
