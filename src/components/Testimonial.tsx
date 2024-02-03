f157f9a96b91c2000856d7381d5c896c32f2431e
Navindu-Abhishek <abi2000navi@gmail.com>
2024-02-04 01:09:56 +0530

    fix issues-2

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -1,6 +1,6 @@
 <style>
   @import
-  url("https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.min.css")
+  url(&quot;https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.min.css&quot;)
 </style>;
 import Image from 'next/image'
 const Testimonial = () => {
