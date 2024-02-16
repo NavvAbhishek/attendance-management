f9fed8fb1d83c2126a2cba632fdbad8bdd97e19e
Navindu-Abhishek <abi2000navi@gmail.com>
2024-02-17 00:43:38 +0530

    update login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import toast from "react-hot-toast";
+import Image from "next/image";
+import Img from "../../../public/login_img.png";
 
 const LoginPage = () => {
   const router = useRouter();
@@ -39,61 +41,66 @@ const LoginPage = () => {
   }, [user]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
-      <h1 className="text-4xl font-bold mb-8">
-        {loading ? "Processing..." : "Login"}
-      </h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100 pattern-bg">
+      <div className="flex md:flex-row flex-col">
+        <div className="mt-10 md:mt-0">
+          <Image
+            src={Img}
+            alt="logo"
+            className="w-[400px] md:w-[500px] h-[550px] object-cover md:rounded-s-lg"
+          />
+        </div>
+        <div className="bg-yellow-100 shadow-xl p-8 h-[550px] w-[400px] md:w-[450px] mb-10 md:mb-0 md:rounded-e-lg">
+          <h1 className="font-bold text-center mb-4 text-4xl">
+            {loading ? "Processing..." : "Login"}
+          </h1>
 
-      <div className="bg-white shadow-xl rounded-lg p-8 max-w-sm w-full">
-        <hr className="mb-6 border-t-2 border-gray-200" />
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700 mb-1"
+          >
+            Email
+          </label>
+          <input
+            className="p-3 border border-gray-300 rounded-lg text-black w-full mb-4 focus:border-blue-500 focus:ring focus:ring-blue-200 transition duration-200"
+            type="text"
+            id="email"
+            value={user.email}
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+            placeholder="Enter your email"
+          />
 
-        <label
-          htmlFor="email"
-          className="block text-sm font-medium text-gray-700 mb-1"
-        >
-          Email
-        </label>
-        <input
-          className="p-3 border border-gray-300 rounded-lg text-black w-full mb-4 focus:border-blue-500 focus:ring focus:ring-blue-200 transition duration-200"
-          type="text"
-          id="email"
-          value={user.email}
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-          placeholder="Enter your email"
-        />
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700 mb-1"
+          >
+            Password
+          </label>
+          <input
+            className="p-3 border border-gray-300 rounded-lg text-black w-full mb-6 focus:border-blue-500 focus:ring focus:ring-blue-200 transition duration-200"
+            type="password"
+            id="password"
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            placeholder="Enter your password"
+          />
 
-        <label
-          htmlFor="password"
-          className="block text-sm font-medium text-gray-700 mb-1"
-        >
-          Password
-        </label>
-        <input
-          className="p-3 border border-gray-300 rounded-lg text-black w-full mb-6 focus:border-blue-500 focus:ring focus:ring-blue-200 transition duration-200"
-          type="password"
-          id="password"
-          value={user.password}
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-          placeholder="Enter your password"
-        />
-
-        <button
-          onClick={onLogin}
-          type="button"
-          className="
-      w-full text-white bg-gradient-to-br from-purple-600 to-blue-500 
-      hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 
-      dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-4
-      transition duration-200 ease-in-out transform hover:scale-105"
-        >
-          {buttonDisabled ? "No Login" : "Login"}
-        </button>
-        <Link
-          href="/signup"
-          className="text-blue-500 hover:text-blue-600 transition duration-200"
-        >
-          Visit Signup page
-        </Link>
+          <button
+            onClick={onLogin}
+            type="button"
+            className="
+            w-full font-medium rounded-lg text-sm blue-button mb-4
+            transition duration-200 ease-in-out transform hover:scale-105"
+          >
+            {buttonDisabled ? "No Login" : "Login"}
+          </button>
+          <Link
+            href="/signup"
+            className="text-light-blue hover:text-dark-blue transition duration-200"
+          >
+            Visit Signup page
+          </Link>
+        </div>
       </div>
     </div>
   );
