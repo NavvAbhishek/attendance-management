7976b4ef152e8c460a7c02fbb6f92880a6b34d85
Navindu-Abhishek <abi2000navi@gmail.com>
2024-02-23 02:53:04 +0530

    restrict attendance-mark page

--- src/app/profile/mark-attendance/page.tsx
+++ src/app/profile/mark-attendance/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import Navbar from "@/components/Navbar";
+import Link from "next/link";
 
 interface ClassData {
   _id: string;
@@ -16,12 +17,32 @@ interface ClassData {
   finishTime: string;
 }
 
+type UserData = {
+  _id: string;
+  role: string;
+};
+
 const MarkAttendence: React.FC = () => {
   const router = useRouter();
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [allClassData, setAllClassData] = useState<ClassData[]>([]);
   const [displayedClassData, setDisplayedClassData] = useState<ClassData[]>([]);
   const [username, setUsername] = useState("nothing");
+  const [userData, setUserData] = useState<UserData | null>(null);
+
+  useLayoutEffect(() => {
+    const getUserDetails = async () => {
+      try {
+        const res = await axios.get("/api/users/me");
+        console.log(res.data);
+        setUserData(res.data.data);
+      } catch (error: any) {
+        console.error(error.message);
+        toast.error(error.message);
+      }
+    };
+    getUserDetails();
+  }, []);
 
   useEffect(() => {
     const getClassesData = async () => {
@@ -91,50 +112,65 @@ const MarkAttendence: React.FC = () => {
 
   return (
     <div>
-      <Navbar />
-      <div>
-        <div className="flex items-center justify-center p-4 mt-10 sm:mt-7">
-          <div className="flex border-2 rounded">
-            <input
-              type="text"
-              className="px-4 py-2 w-80 md:w-96 lg:max-w-md"
-              placeholder="Enter Class ID..."
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-            />
-            <button
-              className="flex items-center justify-center px-4 border-l"
-              onClick={handleSearch}
-            >
-              <FaSearch className="w-4 h-4" />
-            </button>
-          </div>
-        </div>
-
-        <div className="flex flex-col items-center justify-center ">
-          {displayedClassData.map((data) => (
-            <div
-              key={data._id}
-              className="bg-blue-50 w-[50%] sm:w-[45%] md:w-[35%] lg:w-[25%] flex flex-col gap-1 px-5 py-5 mt-5 rounded-md"
-            >
-              <h1>Name - {username}</h1>
-              <h1>Course - {data.course}</h1>
-              <h1>Date - {data.date}</h1>
-              <h1>Start Time - {data.startTime}</h1>
-              <div>
+      {userData?.role === "student" ? (
+        <div>
+          <Navbar />
+          <div>
+            <div className="flex items-center justify-center p-4 mt-10 sm:mt-7">
+              <div className="flex border-2 rounded">
+                <input
+                  type="text"
+                  className="px-4 py-2 w-80 md:w-96 lg:max-w-md"
+                  placeholder="Enter Class ID..."
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
                 <button
-                  className="mark-button text-md font-bold text-dark-yellow px-2 py-[2px] mt-2 rounded-md bg-light-blue"
-                  onClick={() =>
-                    handleClick(data.course, data.date, data.startTime)
-                  }
+                  className="flex items-center justify-center px-4 border-l"
+                  onClick={handleSearch}
                 >
-                  mark
+                  <FaSearch className="w-4 h-4" />
                 </button>
               </div>
             </div>
-          ))}
+
+            <div className="flex flex-col items-center justify-center ">
+              {displayedClassData.map((data) => (
+                <div
+                  key={data._id}
+                  className="bg-blue-50 w-[50%] sm:w-[45%] md:w-[35%] lg:w-[25%] flex flex-col gap-1 px-5 py-5 mt-5 rounded-md"
+                >
+                  <h1>Name - {username}</h1>
+                  <h1>Course - {data.course}</h1>
+                  <h1>Date - {data.date}</h1>
+                  <h1>Start Time - {data.startTime}</h1>
+                  <div>
+                    <button
+                      className="mark-button text-md font-bold text-dark-yellow px-2 py-[2px] mt-2 rounded-md bg-light-blue"
+                      onClick={() =>
+                        handleClick(data.course, data.date, data.startTime)
+                      }
+                    >
+                      mark
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div>
+          <h1 className="text-xl sm:text-2xl font-bold text-red-600 capitalize p-12">
+            You don&apos;t have access to this page!!!
+          </h1>
+          <Link href="/">
+            <button className="text-md font-bold text-red-700 p-2 rounded-md bg-dark-yellow ml-12">
+              Back to Home
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
