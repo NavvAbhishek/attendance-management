efb6322a41f13122011ad2bcfc2dab6ab089f1ce
Navindu-Abhishek <abi2000navi@gmail.com>
2024-03-18 00:39:39 +0530

    add more top icons to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,6 +6,8 @@ import Link from "next/link";
 import SideBar from "@/components/SideBar";
 import { PiStudentBold, PiBooksBold } from "react-icons/pi";
 import { GiTeacher } from "react-icons/gi";
+import { MdRateReview, MdOutlineClass } from "react-icons/md";
+import { BsPeopleFill } from "react-icons/bs";
 import { Chart } from "@/components/Chart";
 import { PieChart } from "@/components/PieChart";
 
@@ -21,10 +23,27 @@ const cardData = [
     number: 40,
   },
   {
+    Icon: MdOutlineClass,
+    title: "Today Classes",
+    number: 18,
+  },
+  {
+    Icon: BsPeopleFill,
+    title: "Weekly Attendance",
+    number: 735,
+  },
+  {
     Icon: PiBooksBold,
     title: "Total Courses",
     number: 32,
   },
+  {
+    Icon: MdRateReview,
+    title: "Attendance Rate",
+    number: "95%",
+    description: "This month",
+  },
+ 
 ];
 
 type UserData = {
@@ -112,13 +131,13 @@ const DashBoard = () => {
           </div>
 
           <div className="w-[100%] md:w-[80%] pt-5">
-            <div className="flex items-center justify-center gap-8">
+            <div className="flex flex-wrap items-center justify-center gap-8 w-[90%] mx-auto">
               {cardData.map(({ Icon, title, number }) => (
                 <div className="flex items-center flex-col" key={title}>
                   <div className="p-3 bg-yellow-400 rounded-lg mb-3">
-                    <Icon className="w-8 h-8 text-normal-blue" />
+                    <Icon className="w-6 h-6 md:w-8 md:h-8 text-normal-blue" />
                   </div>
-                  <p className="font-bold">{title}</p>
+                  <p className="font-bold md:text-md text-sm">{title}</p>
                   <p>{number}</p>
                 </div>
               ))}
