917e733889df070fb42aeea36a93160093ffd9fa
Navindu Abhishek <abi2000navi@gmail.com>
2024-04-28 20:36:38 +0530

    update logout api

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -2,15 +2,21 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     try {
-        const response = NextResponse.json(
-            {
-                message: "Logout successful",
-                success: true,
-            }
-        )
-        response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
+        const response = NextResponse.json({
+            message: "Logout successful",
+            success: true,
+        });
+
+        response.cookies.set("token", "", {
+            path: "/",
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== "development",
+            sameSite: "strict",
+            expires: new Date(0),
+        });
+
         return response;
     } catch (error: any) {
-        return NextResponse.json({ error: error.message }, { status: 500 })
+        return NextResponse.json({ error: error.message }, { status: 500 });
     }
 }
